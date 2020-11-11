7c53c8bbde2ab138e36f4247d223a2706c36985e
MrKalen <mrkalen@hotmail.com>
2020-11-11 09:34:56 -0800

    in progress recipe randomizer issue 2 for user-can-find-random-taco-recipes

--- ajax-project/js/main.js
+++ ajax-project/js/main.js
@@ -9,18 +9,34 @@ tacoApi.send();
 
 // beer API
 
-var recipeApi = new XMLHttpRequest();
-recipeApi.open('GET', 'https://api.punkapi.com/v2/beers/random');
-recipeApi.addEventListener('load', function () {
+var beerApi = new XMLHttpRequest();
+beerApi.open('GET', 'https://api.punkapi.com/v2/beers/random');
+beerApi.addEventListener('load', function () {
 });
-recipeApi.send();
+beerApi.send();
 
 // Recipe cycler
 
 var $recipeWindow = document.querySelector('.recipe-window');
+var $recipe = document.querySelector('.window');
 
 document.addEventListener('click', function () {
+  event.preventDefault();
+  debugger;
+  function recipeRender(recipe) {
+    var recipeName = document.createElement('h2');
+    recipeName.setAttribute('class', 'title');
+    recipeName.textContent = tacoName;
+    $recipe.appendChild(recipeName);
+
+    return recipeName;
+  }
+  var tacoData = tacoApi.response;
+  var tacoName = tacoData.base_layer.name + ' with ' + tacoData.mixin.name + ' and a ' + tacoData.condiment.name + ' in ' + tacoData.shell.name + '.';
+
   if (event.target.getAttribute('id') === 'cycle-recipe') {
+    recipeRender(tacoData);
     console.log('tacoAPI:', tacoApi.response);
+    console.log('taco name:', tacoName);
   }
 });
